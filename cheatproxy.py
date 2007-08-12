02691aaeb24012bd86411431b447c268344d5853
Simon Morgan <sjm@spamcop.net>
2007-08-12 21:26:32 +0000

    Remove Twisted dependency.

--- cheatproxy.py
+++ cheatproxy.py
@@ -1,17 +1,92 @@
 #!/usr/bin/env python
 
+import getopt
+import select
+import socket
 import string
-from urlparse import urlparse
+import urlparse
 
-from twisted.web import proxy, http
-from twisted.internet import reactor
+import BaseHTTPServer
+import SocketServer
 
 from cheatbt import CheatBT
 
-PORT = 3456
+class CheatHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        c = CheatBT()
+        print "Before: " + self.path
+        cheatpath = c.cheat_url(self.path, True)
+        print "After: " + cheatpath
 
-class CheatProxy(proxy.Proxy):
-    def dataReceived(self, data):
+        (scheme, netloc, path, params, query, fragment) = \
+            urlparse.urlparse(cheatpath, 'http')
+
+        if scheme != 'http' or fragment or not netloc:
+            self.send_error(400, "bad url %s" % cheatpath)
+            return
+
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            if self._connect_to(netloc, soc):
+                self.log_request()
+                request = urlparse.urlunparse(('', '', path, params, query, ''))
+                soc.send("%s %s %s\r\n" % (self.command, request,
+                                           self.request_version))
+                self.headers['Connection'] = 'close'
+                del self.headers['Proxy-Connection']
+                for key_val in self.headers.items():
+                    soc.send("%s: %s\r\n" % key_val)
+                soc.send("\r\n")
+                self._read_write(soc)
+        finally:
+            soc.close()
+            self.connection.close()
+
+    def _connect_to(self, netloc, soc):
+        i = netloc.find(':')
+        if i >= 0:
+            host_port = (netloc[:i], int(netloc[i+1:]))
+        else:
+            host_port = (netloc, 80)
+
+        try:
+            soc.connect(host_port)
+        except socket.error, arg:
+            try:
+                msg = arg[1]
+            except:
+                msg = arg
+            self.send_error(404, msg)
+            return False
+
+        return True
+
+    def _read_write(self, soc, max_idling=20):
+        iw = [self.connection, soc]
+        ow = []
+        count = 0
+        while True:
+            count += 1
+            (ins, _, exs) = select.select(iw, ow, iw, 3)
+            if exs:
+                break
+            if ins:
+                for i in ins:
+                    if i is soc:
+                        out = self.connection
+                    else:
+                        out = soc
+                    data = i.recv(8192)
+                    if data:
+                        out.send(data)
+                        count = 0
+            else:
+                pass
+            if count == max_idling:
+                break
+
+        """
         c = CheatBT()
         headers = data.splitlines(True)
         request = headers[0].split()
@@ -19,13 +94,12 @@ class CheatProxy(proxy.Proxy):
         request[2] = request[2] + "\r\n"
         headers[0] = " ".join(request)
         headers = "".join(headers)
-        proxy.Proxy.dataReceived(self, headers)
+        """
 
-#log.startLogging(sys.stdout)
+class CheatServer(SocketServer.ThreadingMixIn,
+                  BaseHTTPServer.HTTPServer):
+    pass
 
 if __name__ == "__main__":
-    factory = http.HTTPFactory()
-    factory.protocol = CheatProxy
-
-    reactor.listenTCP(PORT, factory)
-    reactor.run()
+    httpd = CheatServer(('localhost', 31337), CheatHandler)
+    httpd.serve_forever()
